bcdcec415dd62caf4ba3d5a3a89c493bffc59cda
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-05-28 17:17:30 -0300

    chore: adiciona credentials include nas opcoes globais do auth client

--- fit-ai-frontend/app/_lib/auth-client.ts
+++ fit-ai-frontend/app/_lib/auth-client.ts
@@ -1,8 +1,14 @@
 import { createAuthClient } from "better-auth/react";
 
+// Injeta a URL correta dinamicamente dependendo do ambiente
+const baseURL = typeof window === "undefined"
+  ? (process.env.INTERNAL_API_URL || "http://backend:8081")
+  : (process.env.NEXT_PUBLIC_API_URL || "https://fitai-backend-fdgf.onrender.com");
+
 export const authClient = createAuthClient({
-  // Ao buscar direto do process.env no momento da execução, o Next.js é forçado a pegar o valor atualizado
-  baseURL: typeof window === "undefined"
-    ? (process.env.INTERNAL_API_URL || "http://backend:8081")
-    : (process.env.NEXT_PUBLIC_API_URL || "https://fitai-backend-fdgf.onrender.com"),
+  baseURL: baseURL,
+  
+  fetchOptions: {
+    credentials: "include", 
+  }
 });
